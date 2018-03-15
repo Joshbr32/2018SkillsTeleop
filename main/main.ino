f6d2c4428a606ee75603de6eecdbc91ccdbe2297
Josh Dhori <Joshbr32@gmail.com>
2018-03-15 12:29:58 -0400

    Theoretical pipebot code
    I coded functions for the pipebot, but this code needs to be tested and the values need to be updated with actual values that work before this code will work.

---
+++ main/main.ino
@@ -0,0 +1,16 @@
+/*
+ * Skills 2018 Tele-Op Code v2
+ * @authour Josh Dhori
+ */
+
+#include <Servo.h>;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
